57d0ba769234af78b069dc52fbcf3c8eec7581d9
byzh <805278087@qq.com>
2024-04-16 13:51:56 +0800

    awa

--- main.cpp
+++ main.cpp
@@ -1,6 +1,51 @@
-#include <iostream>
+#include <stdio.h>
+#include "easyx/include/easyx.h"
+#include "easyx/include/graphics.h"
+#include "tools/tools.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#define WIN_WIDTH 900
+#define WIN_HEIGHT 600
+
+enum{WAN_DOU,XIANG_RI_KUI,DA_ZUI_HUA,YING_TAO_ZHA_DAN,PLANT_CNT};
+
+IMAGE imgBg;	//背景图片（这样快）
+IMAGE imgBar;
+IMAGE imgTest;
+
+IMAGE imgCards[PLANT_CNT];
+void gameInit() {
+    // 背景图片
+    // 把字符集修改为“多字节字符集”
+    loadimage(&imgBg, "./res/bg.jpg");
+    loadimage(&imgBar, "./res/bar5.png");
+    loadimage(&imgTest, "./res/Cards/card_1.png");
+    // 植物卡牌
+    char name[64];
+    for (int i = 0; i < PLANT_CNT; i++) {
+        sprintf_s(name, sizeof(name), "./res/Cards/card_%d.png",i+1);
+        loadimage(&imgCards[i], name);
+    }
+    // 创建图形窗口
+    initgraph(WIN_WIDTH, WIN_HEIGHT);
+}
+
+void updateWindow() {
+    putimage(0,0,&imgBg);
+//    putimage(250,0,&imgBar);
+    putimagePNG(250,0,&imgBar);
+
+    for (int i = 0; i < PLANT_CNT; i++) {
+        int x = 338 + i * 65;
+        int y = 6;
+        putimage(x, y, &imgCards[i]);
+    }
+}
+
+int main(void) {
+    gameInit();
+
+    updateWindow();
+
+    system("pause");
     return 0;
 }
---
+++ tools/tools.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "../easyx/include/graphics.h"
+
+void putimagePNG(int  picture_x, int picture_y, IMAGE* picture);
+int getDelay();
